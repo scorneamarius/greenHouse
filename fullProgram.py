869c810e3774ccc6fccf3715f5547ec061ec5be4
RazailaAlexandra <razaila.alexandra@yahoo.com>
2020-05-07 23:03:41 +0300

    database added

--- fullProgram.py
+++ fullProgram.py
@@ -1,10 +1,32 @@
 #!/usr/bin/python
 from gpiozero import DigitalInputDevice
 import time
+import mysql.connector
 import sys
 import Adafruit_DHT
 from picamera import PiCamera
 from time import sleep
+# pentru a putea utiliza baza de date trebuie sa o instalam
+# C:\Users\Your Name\AppData\Local\Programs\Python\Python36-32\Scripts>python -m pip install mysql-connector
+# daca nu exista o baza de date o va crea, iar daca exista e va conecta la ea
+try:
+    database = mysql.connector.connect(
+        host="localhost",
+        user="greenHouse",
+        passwd="greenHouse",
+        mydb="mydatabase"
+        )
+    mycursor = database.cursor()
+except mysql.connector.Error:
+    database = mysql.connector.connect(
+        host="localhost",
+        user="greenHouse",
+        passwd="greenHouse"
+        )
+    mycursor = database.cursor()
+    mycursor.execute("CREATE DATABASE mydatabase")
+    mycursor.execute("CREATE TABLE greenHouseInformation(id INT AUTO_INCREMENT PRIMARY KEY, temperatura INT, umiditate INT)")
+
 camera=PiCamera()
 camera.rotation=180
 camera.start_preview()
@@ -24,7 +46,11 @@ while True:
     else:
         print('Info from 2st sensor:You need to put water!')
     humidity, temperature = Adafruit_DHT.read_retry(11, 4)
-    print 'Temp: {0:0.1f} C  Humidity: {1:0.1f} %'.format(temperature, humidity)    
+    print 'Temp: {0:0.1f} C  Humidity: {1:0.1f} %'.format(temperature, humidity) 
+    sql = "INSERT INTO greenHouseInformation(temperatura, umiditate) VALUES (%d,%d)"   
+    val = (temperature, humidity)
+    mycursor.execute(sql,val)
+    mydb.commit()
     time.sleep(60)
 
 
